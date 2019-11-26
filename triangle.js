4beac4b49623a16fc42841a5c1c4a21023350c8e
John Goytia <JGoytia@ucvts.tec.nj.us>
2019-11-26 13:27:26 -0500

    final

---
+++ triangle.js
@@ -0,0 +1,15 @@
+const readlineSync = require("readline-Sync");
+
+const base = Number(question.readlineSync("Enter base: "));
+const height = Number(question.readlineSync("Enter height: "))
+const hypotenuse = Number(question.readlineSync("Enter hypotenuse: "))
+
+let a = base
+let b = height
+let c = hypotenuse
+
+if (c ** 2 = a ** 2 + b ** 2 ) {
+  console.log("Yes, that's a right triangle!");
+}else {
+  console.log("Nope...not a right triangle.");
+}
